2197708e7ae50f2036756fa875da618d4b92ad66
oiat11 <fu.qia@northeastern.edu>
2024-07-22 16:35:55 -0700

    added the settings button to bottom navigation bar

--- Screens/Home.js
+++ Screens/Home.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Diet from './Diet'; 
 import Activities from './Activities'; 
+import Settings from './Settings';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -17,12 +20,15 @@ export default function Home() {
               return <MaterialIcons name="fastfood" size={size} color={color} />;
             } else if (route.name === 'Activities') {
               return <FontAwesome5 name="running" size={size} color={color} />;
+            } else if (route.name === 'Settings') {
+              return <FontAwesome name="gear" size={size} color={color} />;
             }
           },
         })}
       >
         <Tab.Screen name="Diet" component={Diet} />
         <Tab.Screen name="Activities" component={Activities} />
+        <Tab.Screen name="Settings" component={Settings} /> 
       </Tab.Navigator>
     );
   }
