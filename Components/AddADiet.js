884f8d18153e38e9277e77285658e9b191f17929
oiat11 <fu.qia@northeastern.edu>
2024-07-25 20:47:23 -0700

    added buttons and handleSave function to AddADiet component

--- Components/AddADiet.js
+++ Components/AddADiet.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import CustomTextInput from '../Components/CustomTextInput';
 import NumberInput from './NumberInput';
 import DateComponent from './DateComponent';
+import ActionButtons from './ActionButtons';
+import { writeToDB } from '../Firebase/firestoreHelper';
 
 
 const AddADiet = ({ navigation }) => {
   const [description, setDescription] = useState('');
   const [calories, setCalories] = useState('');
   const [date, setDate] = React.useState(null);
+
+  const handleSave = async () => {
+    if (!description || !calories || !date) {
+      Alert.alert('Error', 'Please fill out all fields');
+    } else if (isNaN(calories )) {
+      Alert.alert('Error', 'Duration must be a number');
+    } else {
+      const newDiet = { description, calories, date: date.toISOString() };
+      try {
+        await writeToDB(newDiet, 'diets');
+        Alert.alert('Success', 'Diet added successfully');
+        navigation.navigate('DietScreen');
+      } catch (error) {
+        console.error('Error adding diet: ', error);
+        Alert.alert('Error', 'There was an error adding the diet');
+      }
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -19,6 +39,7 @@ const AddADiet = ({ navigation }) => {
       />
       <NumberInput label="Calories *" value={calories} onChange={setCalories}/>
       <DateComponent date={date} setDate={setDate} />
+      <ActionButtons onCancel={() => navigation.goBack()} onSave={handleSave} />
     </View>
   );
 };
